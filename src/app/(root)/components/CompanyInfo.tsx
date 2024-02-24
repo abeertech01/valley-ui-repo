f1e52c17d029bc91c55629aa29f30e2c5a7e23c6
abeertech01 <abeer.technology@gmail.com>
2024-02-25 02:34:19 +0600

    mobile responsive

--- src/app/(root)/components/CompanyInfo.tsx
+++ src/app/(root)/components/CompanyInfo.tsx
@@ -2,19 +2,27 @@
 
 import Image from "next/image"
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form"
+import { useState } from "react"
+import { twMerge } from "tailwind-merge"
 
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { useState } from "react"
-import { twMerge } from "tailwind-merge"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 function CompanyInfo() {
   const [selectedBtnValue, setSelectedBtnValue] = useState("1-10")
+  const [selectedFundingRound, setSelectedFundingRound] = useState("seed")
   const {
     register,
-    reset,
+    setValue,
     handleSubmit,
     formState: { errors },
   } = useForm<FieldValues>({
@@ -23,10 +31,18 @@ function CompanyInfo() {
       website: "https://sixteen.life",
       linkedin: "https://www.linkedin.com/company/sixteenlife",
       industry: "Digital Wellbeing",
+      compDesig: "Redesign your digital life, reduce your screen time",
+      compGoals:
+        "Help everyone to be more conscious of where they are spending their time",
+      headQ: "Delhi, India",
+      fundingRound: "seed",
+      faqs: "https://sixteen.life/faq",
     },
   })
 
   const onSubmit: SubmitHandler<FieldValues> = (formData) => {
+    formData.empCount = selectedBtnValue
+    formData.fundingRound = selectedFundingRound
     console.log(formData)
   }
 
@@ -34,12 +50,16 @@ function CompanyInfo() {
     setSelectedBtnValue(value)
   }
 
+  const selectFundingRound = (value: any) => {
+    setSelectedFundingRound(value)
+  }
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="mt-8 p-5">
+    <form onSubmit={handleSubmit(onSubmit)} className="mt-8 lg:p-5">
       {/* THE INTRO LINE - Logo and buttons */}
-      <div className="intro-line flex justify-between mb-12">
+      <div className="intro-line flex flex-col gap-[1.875rem] lg:gap-auto lg:flex-row justify-between mb-12">
         {/* The Logo part in the LEFT SIDE */}
-        <div className="logo-div flex gap-[1.875rem]">
+        <div className="logo-div flex gap-[1.875rem] order-2 lg:order-1">
           <div>
             <Image
               src={"/SVGs/placeholder-comp-logo.svg"}
@@ -69,16 +89,22 @@ function CompanyInfo() {
         </div>
 
         {/* The Saving option in the RIGHT SIDE */}
-        <div className="buttons-div flex gap-3">
-          <Button type="button" variant="outline">
+        <div className="buttons-div w-full lg:w-auto flex gap-3 order-1 lg:order-2">
+          <Button
+            type="button"
+            variant="outline"
+            className="flex-1 lg:flex-auto"
+          >
             Cancel
           </Button>
-          <Button type="submit">Save changes</Button>
+          <Button type="submit" className="flex-1 lg:flex-auto">
+            Save changes
+          </Button>
         </div>
       </div>
 
       {/* COMPANY INFO */}
-      <div className="comp-info grid grid-cols-[repeat(2,1fr)] gap-x-10 gap-y-6 text-sm mb-10">
+      <div className="comp-info flex flex-col gap-7 lg:grid lg:grid-cols-[repeat(2,1fr)] lg:gap-x-10 lg:gap-y-6 text-sm mb-10">
         <label className="text-[#17171FCC]">
           Company's Name
           <Input {...register("name")} className="mt-2" />
@@ -101,7 +127,7 @@ function CompanyInfo() {
       </div>
 
       {/* EMPLOYEE COUNT */}
-      <div className="employee-count">
+      <div className="employee-count mb-10 text-sm">
         <h1 className="text-[##17171F] mb-4">Employee Count</h1>
         <RadioGroup
           defaultValue="1-10"
@@ -173,6 +199,47 @@ function CompanyInfo() {
             </Label>
           </div>
         </RadioGroup>
+      </div>
+
+      {/* COMPANY DESIGNATION */}
+      <div className="comp-designation text-sm mb-10">
+        <h1 className="text-[##17171F] mb-2">Company description</h1>
+        <Input {...register("compDesig")} className="mb-2" />
+        <p className="text-[#17171F99]">Your detailed company description</p>
+      </div>
+
+      {/* COMPANY GOALS */}
+      <div className="comp-goals text-sm mb-10">
+        <h1 className="text-[##17171F] mb-2">What are your company goals?</h1>
+        <Input {...register("compGoals")} className="" />
+      </div>
+
+      {/* HEADQUARTERS */}
+      <div className="head-q text-sm mb-10">
+        <h1 className="text-[##17171F] mb-2">Headquarters</h1>
+        <Input {...register("headQ")} className="" />
+      </div>
+
+      {/* FUNDING ROUND AND FAQs */}
+      <div className="comp-info flex flex-col gap-7 lg:grid lg:grid-cols-[repeat(2,1fr)] lg:gap-x-10 lg:gap-y-6 text-sm mb-10">
+        <label className="text-[#17171FCC]">
+          Funding Rounds
+          <Select defaultValue="seed" onValueChange={selectFundingRound}>
+            <SelectTrigger className="mt-2">
+              <SelectValue />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="seed">Seed</SelectItem>
+              <SelectItem value="seriesa">Series A</SelectItem>
+              <SelectItem value="seriesb">Series B</SelectItem>
+            </SelectContent>
+          </Select>
+        </label>
+
+        <label className="text-[#17171FCC]">
+          FAQs
+          <Input {...register("faqs")} className="mt-2" />
+        </label>
       </div>
     </form>
   )
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,15 +4,21 @@ import CompanyInfo from "./components/CompanyInfo"
 
 export default function Home() {
   return (
-    <main className="max-w-[1204px] mx-auto">
-      <h1 className="text-4xl font-semibold leading-none mb-8">Settings</h1>
-      <Tabs defaultValue="info" className="w-full">
+    <main className="lg:max-w-[1204px] lg:mx-auto mx-6">
+      <h1 className="max-w-[342px] mx-auto lg:max-w-full lg:text-4xl text-[1.75rem] font-semibold leading-none mb-8">
+        Settings
+      </h1>
+      <Tabs defaultValue="info" className="lg:max-w-full max-w-[342px] mx-auto">
         <TabsList>
           <TabsTrigger value="profile">Your Profile</TabsTrigger>
           <TabsTrigger value="info">Company Info</TabsTrigger>
           <TabsTrigger value="manageSeats">Manage Seats</TabsTrigger>
-          <TabsTrigger value="dontContact">Do not Contact</TabsTrigger>
-          <TabsTrigger value="integrations">Integrations</TabsTrigger>
+          <TabsTrigger value="dontContact" className="hidden lg:block">
+            Do not Contact
+          </TabsTrigger>
+          <TabsTrigger value="integrations" className="hidden lg:block">
+            Integrations
+          </TabsTrigger>
         </TabsList>
 
         <TabsContent value="profile">
@@ -22,8 +28,12 @@ export default function Home() {
           <CompanyInfo />
         </TabsContent>
         <TabsContent value="manageSeats">Manage Seats here</TabsContent>
-        <TabsContent value="dontContact">Do not Contact here</TabsContent>
-        <TabsContent value="integrations">Integrations here</TabsContent>
+        <TabsContent value="dontContact" className="hidden lg:block">
+          Do not Contact here
+        </TabsContent>
+        <TabsContent value="integrations" className="hidden lg:block">
+          Integrations here
+        </TabsContent>
       </Tabs>
     </main>
   )
